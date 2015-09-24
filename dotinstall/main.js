94fe2dc13d159f890ad08517766845f26a89c155
mille-f <f11235tetra@gmail.com>
2015-09-25 06:07:30 +0900

    finish #07

--- dotinstall/main.js
+++ dotinstall/main.js
@@ -12,21 +12,19 @@ window.onload = function() {
     bear.y = 0;
 
     bear.addEventListener('enterframe', function() {
-      if ( core.input.left )  this.x -= 5;
-      if ( core.input.right ) this.x += 5;
-      if ( core.input.up )    this.y -= 5;
-      if ( core.input.down )  this.y += 5;
     });
 
-    bear.on('touchstart', function() {
-      core.rootScene.removeChild(this);
+    var label = new Label();
+    label.x = 280;
+    label.y = 5;
+    label.color = 'red';
+    label.font = '14px "Arial"';
+    label.text = '0';
+    label.on('enterframe', function() {
+      label.text = (core.frame / core.fps).toFixed(2);
     });
 
-    core.rootScene.on('touchstart', function(e) {
-      bear.x = e.x;
-      bear.y = e.y;
-    });
-
+    core.rootScene.addChild(label);
     core.rootScene.addChild(bear);
   };
   core.start();
