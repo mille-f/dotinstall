52918839b537918e9a001dbf333f47ba2429d2f6
mille-f <f11235tetra@gmail.com>
2015-09-25 06:04:03 +0900

    finish #06

--- dotinstall/main.js
+++ dotinstall/main.js
@@ -10,14 +10,21 @@ window.onload = function() {
     bear.image = core.assets['chara1.png'];
     bear.x = 0;
     bear.y = 0;
-    bear.frame = 1;
 
     bear.addEventListener('enterframe', function() {
-      this.x += 5;
-      this.frame = this.age % 3 + 10;
-      if ( this.x > 320 ) this.x = 0;
-      //this.rotate(2);
-      //this.scale(1.01,1.01);
+      if ( core.input.left )  this.x -= 5;
+      if ( core.input.right ) this.x += 5;
+      if ( core.input.up )    this.y -= 5;
+      if ( core.input.down )  this.y += 5;
+    });
+
+    bear.on('touchstart', function() {
+      core.rootScene.removeChild(this);
+    });
+
+    core.rootScene.on('touchstart', function(e) {
+      bear.x = e.x;
+      bear.y = e.y;
     });
 
     core.rootScene.addChild(bear);
